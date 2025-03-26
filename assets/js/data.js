befa2c6e2109a063a2f4716718d2746bbca7a070
Ponkoj Mondol <ponkojmonndal2014@gmail.com>
2025-03-26 20:00:31 +0600

    Git initialized

---
+++ assets/js/data.js
@@ -0,0 +1,11 @@
+// Data for navbar
+
+const navbarLinks = [
+  { name: "Home", url: "index.html" },
+  { name: "About", url: "about.html" },
+  { name: "Services", url: "services.html" },
+  { name: "Blog", url: "blog.html" },
+  { name: "Pages", url: "pages.html" },
+  { name: "Contact", url: "contact.html" },
+];
+console.log("This is data.js");
